9786f4fa8066f0b683ba03da56f2238101c98688
aqiuaqiu <35759241+aqiuaqiu@users.noreply.github.com>
2024-06-30 14:09:08 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.jiudaoxian.top": dockerHub,
+  "quay.jiudaoxian.top": "https://quay.io",
+  "gcr.jiudaoxian.top": "https://gcr.io",
+  "k8s-gcr.jiudaoxian.top": "https://k8s.gcr.io",
+  "k8s.jiudaoxian.top": "https://registry.k8s.io",
+  "ghcr.jiudaoxian.top": "https://ghcr.io",
+  "cloudsmith.jiudaoxian.top": "https://docker.cloudsmith.io",
+  "ecr.jiudaoxian.top": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
